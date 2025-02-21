28358e5ff82afed39c694cd3793c9933afd4f3f8
soyoung <soyoung7896@naver.com>
2025-02-21 15:01:21 +0900

    [추가] 01_Scripts_UI 폴더 및 OptionHighlight 스크립트
    텍스트 색상 강조 스크립트 추가

---
+++ Assets/01_Scripts/UI/OptionHighlight.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+using ColorUtility = UnityEngine.ColorUtility;
+
+public class OptionHighlight : MonoBehaviour
+{
+    public string highlightText; // 강조할 문자
+    public TMP_Text optionText; // 문자 TMP
+    public Color highlightColor; // 강조할 색상
+    
+    private void Start()
+    {
+        HighlightSetting();
+    }
+
+    private void HighlightSetting()
+    {
+        if (optionText.text.Contains(highlightText))
+        {
+            string colorCode = ColorUtility.ToHtmlStringRGB(highlightColor);
+            optionText.text = optionText.text.Replace(highlightText, $"<color=#{colorCode}>"+ highlightText + "</color>");
+        }
+
+        else
+        {
+            Debug.Log($"{highlightText} 문자열을 찾을 수 없음");
+        }
+    }
+}
