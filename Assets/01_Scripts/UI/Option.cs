494754a83c158c153cb537b2b364a806dff8fe87
soyoung <soyoung7896@naver.com>
2025-02-21 16:32:23 +0900

    [변경] OptionHighlight -> Option 이름 변경
    아웃라인 색 변경 메서드 추가

--- Assets/01_Scripts/UI/Option.cs
+++ Assets/01_Scripts/UI/Option.cs
@@ -1,18 +1,24 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using ColorUtility = UnityEngine.ColorUtility;
 
-public class OptionHighlight : MonoBehaviour
+public class Option : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public Image outline; // 버튼 충돌시 변경될 아웃라인
+    private Color originalOutlineColor; // 기본 아웃라인 색상
     public string highlightText; // 강조할 문자
     public TMP_Text optionText; // 문자 TMP
     public Color highlightColor; // 강조할 색상
     
     private void Start()
     {
+        originalOutlineColor = outline.color;
         HighlightSetting();
     }
-
+    
     private void HighlightSetting()
     {
         if (optionText.text.Contains(highlightText))
@@ -25,5 +31,15 @@ public class OptionHighlight : MonoBehaviour
         {
             Debug.Log($"{highlightText} 문자열을 찾을 수 없음");
         }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        outline.color = Color.green;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        outline.color = originalOutlineColor;
     }
 }
