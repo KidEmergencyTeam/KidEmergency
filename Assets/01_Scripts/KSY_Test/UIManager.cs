885ccc69967e80909e7e5f225424636e92422c5e
ztazan2 <ztazan2@gamil.com>
2025-02-21 12:04:38 +0900

    [수정] 메시지 출력 -> 오브젝트 잡았을때 3초간 유지되는 메시지 창 출력

--- Assets/01_Scripts/KSY_Test/UIManager.cs
+++ Assets/01_Scripts/KSY_Test/UIManager.cs
@@ -4,25 +4,24 @@ using TMPro;
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
-    public TextMeshProUGUI explanationText;
+    public GameObject explanationPanel;       // 패널 (비활성화되어 있음)
+    public TextMeshProUGUI explanationText;     // 패널 안에 있는 텍스트
 
     void Awake()
     {
         instance = this;
     }
 
+    // 메시지에 따라 텍스트를 업데이트하고 패널을 활성화합니다.
     public void ShowExplanation(string message)
     {
-        explanationText.text = message;
-        explanationText.gameObject.SetActive(true);
-
-        // 메시지 출력후
-        // 3초 후 비활성화
-        Invoke("HideExplanation", 3f);  
+        explanationText.text = message;       // 텍스트 내용 변경
+        explanationPanel.SetActive(true);     // 패널 활성화
+        Invoke("HideExplanation", 3f);        // 3초 후 패널 비활성화
     }
 
     void HideExplanation()
     {
-        explanationText.gameObject.SetActive(false);
+        explanationPanel.SetActive(false);    // 패널 비활성화
     }
 }
