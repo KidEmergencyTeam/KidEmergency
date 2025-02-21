344f4c98beeabac6f22d97e5a005bc3fc47450e9
soyoung <soyoung7896@naver.com>
2025-02-21 18:05:04 +0900

    [추가] TitleMenu
    아웃라인 추가. vr worldspace 변경시 수정 예정

---
+++ Assets/01_Scripts/UI/TitleMenu.cs
@@ -0,0 +1,27 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using ColorUtility = UnityEngine.ColorUtility;
+
+public class TitleMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Image outline; // 버튼 충돌시 변경될 아웃라인
+    private Color originalOutlineColor; // 기본 아웃라인 색상
+    private void Start()
+    {
+        originalOutlineColor = outline.color;
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        outline.color = Color.green;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        outline.color = originalOutlineColor;
+    }
+}
