1d581724a2038a88857fdee4e098560fbd89e5b1
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-02-23 21:06:33 +0900

    [추가] HardwareRig 생성

---
+++ Assets/01_Scripts/Photon/HardwareRig.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using Fusion;
+
+public struct RigInput : INetworkInput
+{
+	public Vector3 headPosition;
+	public Quaternion headRotation;
+	public Vector3 leftHandPosition;
+	public Quaternion leftHandRotation;
+	public Vector3 rightHandPosition;
+	public Quaternion rightHandRotation;
+}
+
+public class HardwareRig : MonoBehaviour
+{
+	[Header("VR References")]
+	public Transform headset;
+	public Transform leftController;
+	public Transform rightController;
+    
+	private NetworkRunner _runner;
+	private Player _Player;
+
+	private void Start()
+	{
+		_runner = FindObjectOfType<NetworkRunner>();
+	}
+
+	public void OnInput(NetworkRunner runner, NetworkInput input)
+	{
+		var rigInput = new RigInput
+		{
+			headPosition = headset.position,
+			headRotation = headset.rotation,
+			leftHandPosition = leftController.position,
+			leftHandRotation = leftController.rotation,
+			rightHandPosition = rightController.position,
+			rightHandRotation = rightController.rotation
+		};
+
+		input.Set(rigInput);
+	}
+}
