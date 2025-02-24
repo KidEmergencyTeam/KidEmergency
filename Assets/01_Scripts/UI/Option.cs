f534a904a5205f17531be23f4bb8618c1e383494
soyoung <soyoung7896@naver.com>
2025-02-24 14:54:09 +0900

    [수정] Option Script
    특정 단어 폰트 크기 변경 추가

--- Assets/01_Scripts/UI/Option.cs
+++ Assets/01_Scripts/UI/Option.cs
@@ -21,8 +21,9 @@ public class Option : OutlineHighlight
         if (_optionText.text.Contains(highlightText))
         {
             string colorCode = ColorUtility.ToHtmlStringRGB(highlightColor);
-            _optionText.text = _optionText.text.Replace(highlightText, $"<b><color=#{colorCode}>"+ highlightText + "</color></b>");
-            // 색상 변경 <color=컬러코드></color> / 폰트 굵기 <b></b> / 폰트 변경(Legacy만 가능) <font=\"폰트명\"><font>
+            _optionText.text = _optionText.text.Replace(highlightText, $"<b><color=#{colorCode}><size=\"40\">"+ highlightText + "</size></color></b>");
+            // 색상 변경 <color=#컬러코드></color> / 폰트 굵기 <b></b> / 폰트 변경(Legacy만 가능) <font=\"폰트명\"><font>
+            // 폰트 사이즈 변경 <size=\"변경할 크기\"></size>
         }
 
         else
