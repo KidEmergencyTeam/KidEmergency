baa5ed62ccf45dbea3b75b5e2fad86a2d6cd6390
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-02-26 17:28:39 +0900

    [수정] HardwareRig 수정

--- Assets/01_Scripts/Photon/HardwareRig.cs
+++ Assets/01_Scripts/Photon/HardwareRig.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
+using Fusion.Sockets;
 
 public enum RigPart
 {
@@ -19,7 +22,7 @@ public struct RigInput : INetworkInput
 	public Quaternion rightHandRotation;
 }
 
-public class HardwareRig : MonoBehaviour
+public class HardwareRig : MonoBehaviour, INetworkRunnerCallbacks
 {
 	[Header("VR References")]
 	public Transform headset;
@@ -48,4 +51,27 @@ public class HardwareRig : MonoBehaviour
 
 		input.Set(rigInput);
 	}
+	#region Unuse INetworkRunnerCallbacks
+	public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+	public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+	public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) { }
+	public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+	public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+	public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
+	public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+	public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress,
+		NetConnectFailedReason reason) { }
+	public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+	public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key,
+		ArraySegment<byte> data) { }
+	public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key,
+		float progress) { }
+	public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+	public void OnConnectedToServer(NetworkRunner runner) { }
+	public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+	public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+	public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+	public void OnSceneLoadDone(NetworkRunner runner) { }
+	public void OnSceneLoadStart(NetworkRunner runner) { }
+	#endregion
 }
