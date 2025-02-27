f95af0bacc2c81d882953299d72bf34ef75f2d08
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-02-27 16:57:10 +0900

    [수정] HardwareHand 수정

--- Assets/01_Scripts/Photon/HardwareHand.cs
+++ Assets/01_Scripts/Photon/HardwareHand.cs
@@ -1,27 +1,14 @@
-using System;
 using Fusion;
 using UnityEngine;
-
-[Serializable]
-public struct HandCommand : INetworkStruct
-{
-    public int poseCommand;
-}
 
 public class HardwareHand : MonoBehaviour
 {
     public RigPart side;
     public NetworkTransform networkTransform;
-    public HandCommand handCommand;
 
     public int handPose = 0;
     private void Awake()
     {
         networkTransform = GetComponent<NetworkTransform>();
-    }
-
-    protected void Update()
-    {
-        handCommand.poseCommand = handPose;
     }
 }
