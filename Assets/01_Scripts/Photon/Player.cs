484c86bebdef2172120d7d6f0e5075bb1ea48947
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-02-28 14:13:03 +0900

    [수정] Player 불필요한 내용 제거

--- Assets/01_Scripts/Photon/Player.cs
+++ Assets/01_Scripts/Photon/Player.cs
@@ -3,25 +3,5 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
-    [Header("Player Parts")]
-    public Transform headTransform;
-    public Transform leftHandTransform;
-    public Transform rightHandTransform;
-
     [Networked] public int Token {get;set;}
-    [Header("Foot Settings")]
-    public float footOffset = 0.3f;
-    public float footHeight = 0.1f;
-
-    private HardwareRig _hardwareRig;
-
-    public override void Spawned()
-    {
-        if (Object.HasInputAuthority) 
-        {
-            _hardwareRig = FindObjectOfType<HardwareRig>();
-            if (_hardwareRig == null)
-                Debug.LogError("HardwareRig를 찾을 수 없습니다!");
-        }
-    }
 }
