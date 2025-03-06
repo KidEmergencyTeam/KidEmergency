21b7cdd831fd6959339e73e360539f57276f8b20
ztazan2 <ztazan2@gamil.com>
2025-03-06 17:38:47 +0900

    [수정] TestButton2.cs 주석 정리

--- Assets/01_Scripts/KSY_Test/TestBuuton2.cs
+++ Assets/01_Scripts/KSY_Test/TestBuuton2.cs
@@ -23,6 +23,7 @@ public class TestButton2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
 
     void Start()
     {
+        // 버튼에 이벤트 등록
         if (button != null)
         {
             button.onClick.AddListener(OnButtonClick);
@@ -35,12 +36,22 @@ public class TestButton2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
 
     void OnEnable()
     {
+        // XRI Default Input Actions 할당되어 있는지 확인
         if (inputActionAsset != null)
         {
+            // XRI Default Input Actions에서
+            // selectActionName에 해당하는 액션을 찾음.
+            // 해당 액션이 없으면 LogError 출력
             selectAction = inputActionAsset.FindAction(selectActionName, true);
+
+            // selectActionName에 해당하는 액션을 찾았을 경우
             if (selectAction != null)
             {
+                // selectAction이 수행되었을 때
+                // OnSelectPerformed 함수를 호출하도록 이벤트 구독
                 selectAction.performed += OnSelectPerformed;
+
+                // selectAction을 활성화
                 selectAction.Enable();
             }
             else
@@ -56,13 +67,21 @@ public class TestButton2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
 
     void OnDisable()
     {
+        // selectAction이 null이 아닌 경우에만
+        // 이벤트 구독 해제 및 비활성화 처리
         if (selectAction != null)
         {
+            // OnSelectPerformed 이벤트 구독 해제
             selectAction.performed -= OnSelectPerformed;
+            // selectAction을 비활성화
             selectAction.Disable();
         }
     }
 
+    // 레이가 버튼 위에 있는 경우
+    // XR 컨트롤러 그립 입력이 발생하면
+    // 1.button.onClick.Invoke()를 호출
+    // 2.버튼 클릭 이벤트를 실행
     private void OnSelectPerformed(InputAction.CallbackContext context)
     {
         if (isRayHovering)
@@ -71,16 +90,19 @@ public class TestButton2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         }
     }
 
+    // 레이가 버튼 위에 있을 경우
     public void OnPointerEnter(PointerEventData eventData)
     {
         isRayHovering = true;
     }
 
+    // 레이가 버튼 위에 없을 경우
     public void OnPointerExit(PointerEventData eventData)
     {
         isRayHovering = false;
     }
 
+    // 버튼 클릭 이벤트 
     public void OnButtonClick()
     {
         Debug.Log("[TestButton2] OnButtonClick() 호출됨.");
