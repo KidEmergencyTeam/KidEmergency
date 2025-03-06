4c36a1f3a2ae7b97c47690d11af6ca0a74e8e4f2
ztazan2 <ztazan2@gamil.com>
2025-03-06 17:10:25 +0900

    [수정] TestButton2.cs 오타 수정, 주석 정리

--- Assets/01_Scripts/KSY_Test/TestBuuton2.cs
+++ Assets/01_Scripts/KSY_Test/TestBuuton2.cs
@@ -3,13 +3,22 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
-public class TestBuuton2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class TestButton2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [Header("버튼")]
     public Button button;
+
+    [Header("XRI Default Input Actions")]
     public InputActionAsset inputActionAsset;
+
+    // Select을 기본값으로 사용
+    [Header("XRI Default Input Actions 내에서 Select(Grip) 액션의 이름")]
     public string selectActionName = "Select";
 
+    // Select 액션을 저장하는 용도 -> 효율성을 위한 개념
     private InputAction selectAction;
+
+    // 레이가 버튼 위에 있는지 여부
     private bool isRayHovering = false;
 
     void Start()
@@ -20,7 +29,7 @@ public class TestBuuton2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         }
         else
         {
-            Debug.LogError("[TestBuuton2] Button이 할당되지 않았습니다.");
+            Debug.LogError("[TestButton2] Button이 할당되지 않았습니다.");
         }
     }
 
@@ -36,12 +45,12 @@ public class TestBuuton2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
             }
             else
             {
-                Debug.LogError("[TestBuuton2] '" + selectActionName + "' 액션을 찾을 수 없습니다.");
+                Debug.LogError("[TestButton2] '" + selectActionName + "' 액션을 찾을 수 없습니다.");
             }
         }
         else
         {
-            Debug.LogError("[TestBuuton2] InputActionAsset이 할당되지 않았습니다.");
+            Debug.LogError("[TestButton2] InputActionAsset이 할당되지 않았습니다.");
         }
     }
 
@@ -74,6 +83,6 @@ public class TestBuuton2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
 
     public void OnButtonClick()
     {
-        Debug.Log("[TestBuuton2] OnButtonClick() 호출됨.");
+        Debug.Log("[TestButton2] OnButtonClick() 호출됨.");
     }
 }
