9f4f176024859be7958b3303f91c53395842f72a
JiwonEE2 <jiwonchichi1@gmail.com>
2025-03-08 20:45:49 +0900

    [추가] StateData
    분리

---
+++ Assets/01_Scripts/Player/StateData.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+
+// 플레이어가 수행하는 행동 상태 정의
+public enum PlayerState
+{
+	None,
+	Button,
+	Pick,
+	Hold,
+	Walk,
+	Push,
+	Bow
+}
+
+// 특정 시나리오별 플레이어 상태 흐름을 저장하는 데이터 클래스
+public static class ModeStateData
+{
+	public static readonly Dictionary<string, List<PlayerState>> ModeSequences =
+		new Dictionary<string, List<PlayerState>>
+		{
+			{
+				"FireKinder",
+				new List<PlayerState>
+				{
+					PlayerState.None, PlayerState.Button, PlayerState.Pick,
+					PlayerState.Hold, PlayerState.Walk, PlayerState.Button,
+					PlayerState.Bow, PlayerState.Button
+				}
+			},
+			{
+				"FireSchool",
+				new List<PlayerState>
+				{
+					PlayerState.None, PlayerState.Button, PlayerState.Pick,
+					PlayerState.Hold, PlayerState.Walk, PlayerState.Push, PlayerState.Bow,
+					PlayerState.Button, PlayerState.Button
+				}
+			}
+		};
+}
