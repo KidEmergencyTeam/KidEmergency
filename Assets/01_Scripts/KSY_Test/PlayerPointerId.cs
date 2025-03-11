7c27b6451dc6ceed1d043fb5114c282419f68ee2
ztazan2 <ztazan2@gamil.com>
2025-03-11 20:13:45 +0900

    [수정] PlayerPointerId.cs
    열거형으로 처리

--- Assets/01_Scripts/KSY_Test/PlayerPointerId.cs
+++ Assets/01_Scripts/KSY_Test/PlayerPointerId.cs
@@ -2,9 +2,48 @@ using UnityEngine;
 
 public class PlayerPointerId : MonoBehaviour
 {
+    // 좌측 포인터 ID 열거형
+    public enum LeftPointerId
+    {
+        None = -1,
+        LeftPointer0,
+        LeftPointer1,
+        LeftPointer2,
+        LeftPointer3,
+        LeftPointer4,
+        LeftPointer5,
+        LeftPointer6,
+        LeftPointer7,
+        LeftPointer8,
+        LeftPointer9,
+        LeftPointer10,
+
+
+        // 필요한 만큼 추가 가능
+    }
+
+    // 우측 포인터 ID 열거형
+    public enum RightPointerId
+    {
+        None = -1,
+        RightPointer0,
+        RightPointer1,
+        RightPointer2,
+        RightPointer3,
+        RightPointer4,
+        RightPointer5,
+        RightPointer6,
+        RightPointer7,
+        RightPointer8,
+        RightPointer9,
+        RightPointer10,
+
+        // 필요한 만큼 추가 가능
+    }
+
     [Header("좌측 포인터 ID")]
-    public int[] leftPointerIds = new int[] { };
+    public LeftPointerId[] leftPointerIds = new LeftPointerId[] { LeftPointerId.LeftPointer0 };
 
     [Header("우측 포인터 ID")]
-    public int[] rightPointerIds = new int[] { };
+    public RightPointerId[] rightPointerIds = new RightPointerId[] { RightPointerId.RightPointer0 };
 }
