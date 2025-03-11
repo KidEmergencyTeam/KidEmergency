56cb036a70ce17d73e31c93e7746efb01c60aa3c
ztazan2 <ztazan2@gamil.com>
2025-03-11 13:33:41 +0900

    [추가] PlayerPointerId.cs
    PlayerPointerId에 좌측과 우측 포인터 ID 배열을 정의하고, 이를 활용하여 어떤 컨트롤러의 레이가 버튼 위에 있는지 식별

---
+++ Assets/01_Scripts/KSY_Test/PlayerPointerId.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerPointerId : MonoBehaviour
+{
+    [Header("좌측 포인터 ID")]
+    public int[] leftPointerIds = new int[] { 2, 9 };
+
+    [Header("우측 포인터 ID")]
+    public int[] rightPointerIds = new int[] { 5, 3 };
+}
