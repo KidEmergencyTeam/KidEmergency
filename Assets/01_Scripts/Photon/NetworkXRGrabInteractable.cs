004e8d797eb093c712c0a537ede729773a7db707
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-03-13 17:12:46 +0900

    [수정] NetworkXRGrabInteractable 수정

--- Assets/01_Scripts/Photon/NetworkXRGrabInteractable.cs
+++ Assets/01_Scripts/Photon/NetworkXRGrabInteractable.cs
@@ -9,22 +9,16 @@ using UnityEngine.XR.Interaction.Toolkit;
 [RequireComponent(typeof(Rigidbody))]
 public class NetworkedXRGrabInteractable : NetworkBehaviour
 {
-	[Networked] public Vector3 Pos { get; set; }
-	[Networked] public Quaternion Rot { get; set; }
 	[Networked] public NetworkBool IsGrabbed { get; set; } = false;
 
 	private XRGrabInteractable _grabInteractable;
-	private NetworkObject _networkObject;
 	private Rigidbody _rb;
-	private NetworkRigidbody3D _networkRb;
 	private GameObject _currentInteractor;
 
 	private void Awake()
 	{
 		_grabInteractable = GetComponent<XRGrabInteractable>();
-		_networkObject = GetComponent<NetworkObject>();
 		_rb = GetComponent<Rigidbody>();
-		_networkRb = GetComponent<NetworkRigidbody3D>();
 
 		_grabInteractable.selectEntered.AddListener(OnGrab);
 		_grabInteractable.selectExited.AddListener(OnRelease);
@@ -32,21 +26,25 @@ public class NetworkedXRGrabInteractable : NetworkBehaviour
 
 	private void OnGrab(SelectEnterEventArgs args)
 	{
-		Runner.SetIsSimulated(Object, true);
-		IsGrabbed = true;
-		_rb.isKinematic = true;
-		_currentInteractor = args.interactorObject.transform.gameObject;
-		Object.AssignInputAuthority(_currentInteractor
-			.GetComponentInParent<NetworkObject>().InputAuthority);
+		if (Object.HasStateAuthority || Object.HasInputAuthority)
+		{
+			if (!Object.HasInputAuthority)
+			{
+				Object.AssignInputAuthority(Object.InputAuthority);
+			}
+
+			_rb.isKinematic = true;
+		}
 	}
 
 	private void OnRelease(SelectExitEventArgs args)
 	{
-		Runner.SetIsSimulated(Object, false);
-		_rb.isKinematic = false;
-		IsGrabbed = false;
-		_currentInteractor = null;
-		Object.RemoveInputAuthority();
+		if (Object.HasInputAuthority)
+		{
+			Object.RemoveInputAuthority();
+
+			_rb.isKinematic = false;
+		}
 	}
 
 	public override void FixedUpdateNetwork()
