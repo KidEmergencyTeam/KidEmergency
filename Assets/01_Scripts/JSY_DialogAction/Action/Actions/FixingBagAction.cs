f6ab67e579d49092fcc24830a268e150f5f7d30f
soyoung <soyoung7896@naver.com>
2025-03-13 14:45:02 +0900

    [수정] FixingBagAction

--- Assets/01_Scripts/JSY_DialogAction/Action/Actions/FixingBagAction.cs
+++ Assets/01_Scripts/JSY_DialogAction/Action/Actions/FixingBagAction.cs
@@ -1,11 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Properties;
 using UnityEngine;
 
 public class FixingBagAction : MonoBehaviour, IActionEffect
 {
     private bool _isComplete = false;
+    [SerializeField] private GameObject bag;
     public bool IsActionComplete => _isComplete;
+
+    private void Start()
+    {
+        StartCoroutine(FindBagObject());
+    }
+
+    private void Update()
+    {
+        if (bag != null)
+        {
+            StopCoroutine(FindBagObject());
+        }
+    }
 
     public void StartAction()
     {
@@ -14,9 +30,18 @@ public class FixingBagAction : MonoBehaviour, IActionEffect
         // _isComplete = true; // 테스트용. 기능 제대로 구현하면 이동 예정
     }
 
-
+    private IEnumerator FindBagObject()
+    {
+        while (bag == null)
+        {
+            print("가방이 없음 가방 찾는 중");
+            bag = GameObject.Find("Backpack");
+            yield return new WaitForSeconds(2f);
+        }
+    }
+    
     private void TestAction()
     {
-        print("가방 고정됨");
+        bag.transform.position = Vector3.zero;
     }
 }
