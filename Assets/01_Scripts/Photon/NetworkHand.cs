a353f961e9432d59ea56f09dbb88cc9c6171c232
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-03-13 15:58:12 +0900

    [수정] NetworkHand 수정

--- Assets/01_Scripts/Photon/NetworkHand.cs
+++ Assets/01_Scripts/Photon/NetworkHand.cs
@@ -1,15 +1,20 @@
 using Fusion;
+using UnityEngine;
 
+[DefaultExecutionOrder(NetworkHand.ExecutionOrder)]
 public class NetworkHand : NetworkBehaviour
 {
-    public NetworkTransform networkTransform;
+	public const int ExecutionOrder = NetworkRig.ExecutionOrder + 10;
+	public NetworkTransform networkTransform;
 
-    public RigPart side;
-    private NetworkRig _rig;
+	public RigPart side;
+	private NetworkRig _rig;
 
-    private void Awake()
-    {
-        _rig = GetComponentInParent<NetworkRig>();
-        networkTransform = GetComponent<NetworkTransform>();
-    }
+	public bool IsLocalNetworkRig => _rig.IsLocalNetworkRig;
+
+	private void Awake()
+	{
+		_rig = GetComponentInParent<NetworkRig>();
+		networkTransform = GetComponent<NetworkTransform>();
+	}
 }
