16181fdfb71dd8e166a6dfa44aa090d5d065c588
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-03-14 17:31:59 +0900

    [수정] NetworkXRGrabinteractable 수정

--- Assets/01_Scripts/Photon/NetworkXRGrabInteractable.cs
+++ Assets/01_Scripts/Photon/NetworkXRGrabInteractable.cs
@@ -10,23 +10,44 @@ public class NetworkGrabbable : NetworkBehaviour
 	private NetworkRigidbody3D m_NetworkRigidbody;
 	private PlayerRef m_OriginalOwner;
 	private bool m_IsGrabbed = false;
+	private Rigidbody m_Rigidbody;
 
 	// 디버깅용 로그 활성화
 	[SerializeField] private bool m_DebugLog = true;
+	
+	// 네트워크 변수 추가 - 물리 상태 동기화를 위해
+	[Networked] private Vector3 NetworkPosition { get; set; }
+	[Networked] private Quaternion NetworkRotation { get; set; }
+	[Networked] private Vector3 NetworkVelocity { get; set; }
+	[Networked] private Vector3 NetworkAngularVelocity { get; set; }
+	[Networked] private NetworkBool IsKinematic { get; set; }
 
-	public override void Spawned()
+	private void Awake()
 	{
-		// 네트워크 오브젝트가 스폰될 때 초기화
 		m_GrabInteractable = GetComponent<XRGrabInteractable>();
 		m_NetworkObject = GetComponent<NetworkObject>();
 		m_NetworkRigidbody = GetComponent<NetworkRigidbody3D>();
+		m_Rigidbody = GetComponent<Rigidbody>();
 		
 		// XRGrabInteractable 이벤트 등록
 		m_GrabInteractable.selectEntered.AddListener(OnGrabbed);
 		m_GrabInteractable.selectExited.AddListener(OnReleased);
-		
+	}
+
+	public override void Spawned()
+	{
 		// 초기 소유자 저장
 		m_OriginalOwner = Object.HasStateAuthority ? Runner.LocalPlayer : Object.StateAuthority;
+		
+		// 초기 네트워크 상태 설정
+		if (Object.HasStateAuthority)
+		{
+			NetworkPosition = transform.position;
+			NetworkRotation = transform.rotation;
+			NetworkVelocity = m_Rigidbody.velocity;
+			NetworkAngularVelocity = m_Rigidbody.angularVelocity;
+			IsKinematic = m_Rigidbody.isKinematic;
+		}
 		
 		if (m_DebugLog)
 			Debug.Log($"[{name}] Spawned. StateAuthority: {Object.StateAuthority}, HasInputAuth: {Object.HasInputAuthority}");
@@ -85,6 +106,16 @@ public class NetworkGrabbable : NetworkBehaviour
 			if (m_DebugLog)
 				Debug.Log($"[{name}] Returning ownership to {m_OriginalOwner}");
 			
+			// 던지기 후 최종 상태 동기화
+			if (Object.HasStateAuthority)
+			{
+				NetworkPosition = transform.position;
+				NetworkRotation = transform.rotation;
+				NetworkVelocity = m_Rigidbody.velocity;
+				NetworkAngularVelocity = m_Rigidbody.angularVelocity;
+				IsKinematic = m_Rigidbody.isKinematic;
+			}
+			
 			RPC_ReturnObjectOwnership(m_OriginalOwner);
 		}
 	}
@@ -100,23 +131,62 @@ public class NetworkGrabbable : NetworkBehaviour
 		// 2. 호스트인 경우 추가 확인
 		if (Runner.IsServer || Runner.IsSharedModeMasterClient)
 		{
-			// 호스트의 인터랙터인지 확인하는 추가 로직
-			// 호스트의 인터랙터에 특별한 태그나 컴포넌트가 있다면 확인
-			// 예시로 간단하게 호스트일 때는 모든 인터랙터를 로컬로 간주
+			// 호스트의 인터랙터인지 확인
+			// 여기서는 간단히 호스트일 때 모든 인터랙터를 로컬로 간주
 			return true;
 		}
 		
 		return false;
 	}
 
-	// 네트워크 상태 동기화를 위한 추가 메서드
+	// 네트워크 상태 동기화를 위한 메서드
 	public override void FixedUpdateNetwork()
 	{
-		// 소유자만 물리 상태를 업데이트
-		if (Object.HasInputAuthority && m_IsGrabbed)
+		// 소유자일 때 - 물리 상태를 네트워크에 동기화
+		if (Object.HasStateAuthority)
 		{
-			// 필요한 경우 추가 동기화 로직
-			// XRGrabInteractable이 이미 위치/회전을 처리하므로 여기서는 추가 작업 불필요
+			if (m_IsGrabbed && Object.HasInputAuthority)
+			{
+				// 잡고 있을 때 위치/회전/속도 업데이트
+				NetworkPosition = transform.position;
+				NetworkRotation = transform.rotation;
+				NetworkVelocity = m_Rigidbody.velocity;
+				NetworkAngularVelocity = m_Rigidbody.angularVelocity;
+				IsKinematic = m_Rigidbody.isKinematic;
+			}
+			else if (!m_IsGrabbed)
+			{
+				// 잡고 있지 않을 때도 물리 상태 업데이트
+				NetworkPosition = transform.position;
+				NetworkRotation = transform.rotation;
+				NetworkVelocity = m_Rigidbody.velocity;
+				NetworkAngularVelocity = m_Rigidbody.angularVelocity;
+				IsKinematic = m_Rigidbody.isKinematic;
+			}
+		}
+		// 소유자가 아닐 때 - 네트워크 값으로 업데이트
+		else if (!m_IsGrabbed)
+		{
+			// 잡고 있지 않을 때만 네트워크 값으로 업데이트
+			transform.position = NetworkPosition;
+			transform.rotation = NetworkRotation;
+			m_Rigidbody.velocity = NetworkVelocity;
+			m_Rigidbody.angularVelocity = NetworkAngularVelocity;
+			m_Rigidbody.isKinematic = IsKinematic;
+		}
+	}
+
+	// 물리 상태 강제 동기화 RPC
+	[Rpc(RpcSources.InputAuthority, RpcTargets.All)]
+	private void RPC_SyncPhysicsState(Vector3 position, Quaternion rotation, Vector3 velocity, Vector3 angularVelocity, NetworkBool isKinematic)
+	{
+		if (!Object.HasInputAuthority)
+		{
+			transform.position = position;
+			transform.rotation = rotation;
+			m_Rigidbody.velocity = velocity;
+			m_Rigidbody.angularVelocity = angularVelocity;
+			m_Rigidbody.isKinematic = isKinematic;
 		}
 	}
 
@@ -142,6 +212,8 @@ public class NetworkGrabbable : NetworkBehaviour
 			if (m_DebugLog)
 				Debug.Log($"[{name}] RPC_ReturnObjectOwnership to {originalOwner}");
 			
+			// 물리 상태 동기화 후 소유권 변경
+			RPC_SyncPhysicsState(transform.position, transform.rotation, m_Rigidbody.velocity, m_Rigidbody.angularVelocity, m_Rigidbody.isKinematic);
 			Object.AssignInputAuthority(originalOwner);
 		}
 	}
@@ -160,5 +232,26 @@ public class NetworkGrabbable : NetworkBehaviour
 			m_GrabInteractable.trackRotation = true;
 			m_GrabInteractable.throwOnDetach = true;
 		}
+	}
+	
+	// 물리 상태 변경 시 강제 동기화
+	private void LateUpdate()
+	{
+		// 소유자이고 물리 상태가 변경되었을 때 강제 동기화
+		if (Object.HasInputAuthority && !m_IsGrabbed && HasPhysicsChanged())
+		{
+			RPC_SyncPhysicsState(transform.position, transform.rotation, m_Rigidbody.velocity, m_Rigidbody.angularVelocity, m_Rigidbody.isKinematic);
+		}
+	}
+	
+	// 물리 상태 변경 감지
+	private bool HasPhysicsChanged()
+	{
+		// 위치, 회전, 속도 등이 네트워크 값과 다른지 확인
+		return Vector3.Distance(transform.position, NetworkPosition) > 0.01f ||
+			   Quaternion.Angle(transform.rotation, NetworkRotation) > 0.5f ||
+			   Vector3.Distance(m_Rigidbody.velocity, NetworkVelocity) > 0.1f ||
+			   Vector3.Distance(m_Rigidbody.angularVelocity, NetworkAngularVelocity) > 0.1f ||
+			   m_Rigidbody.isKinematic != IsKinematic;
 	}
 }
