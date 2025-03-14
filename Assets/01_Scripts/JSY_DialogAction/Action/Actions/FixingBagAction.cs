b7c655921cf9db0b3a54aa1ca467068f96b007cd
soyoung <soyoung7896@naver.com>
2025-03-14 13:44:16 +0900

    [수정] FixingBagAction
    테스트용

--- Assets/01_Scripts/JSY_DialogAction/Action/Actions/FixingBagAction.cs
+++ Assets/01_Scripts/JSY_DialogAction/Action/Actions/FixingBagAction.cs
@@ -1,48 +1,63 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Unity.Properties;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.Serialization;
+using XRController = UnityEngine.XR.Interaction.Toolkit.XRController;
 
 public class FixingBagAction : MonoBehaviour, IActionEffect
 {
     private bool _isComplete = false;
-    [SerializeField] private GameObject bag;
+    [SerializeField] private GameObject[] _bag;
+    private GameObject _xrParent;
+    private Vector3 _fixPos;
     public bool IsActionComplete => _isComplete;
 
     private void Start()
     {
-        StartCoroutine(FindBagObject());
-    }
-
-    private void Update()
-    {
-        if (bag != null)
-        {
-            StopCoroutine(FindBagObject());
-        }
+        // 멀티일 시 고유 ID로 먼저 찾고 Canparent 찾기
+        _xrParent = GameObject.Find("CamParent");
     }
 
     public void StartAction()
     {
         _isComplete = false;
-        TestAction();
-        _isComplete = true; // 테스트용. 기능 제대로 구현하면 이동 예정
+        StartCoroutine(TestAction());
     }
 
-    private IEnumerator FindBagObject()
+    public void StartMultiModeAction()
     {
-        while (bag == null)
-        {
-            print("가방이 없음 가방 찾는 중");
-            bag = GameObject.Find("Backpack");
-            yield return new WaitForSeconds(2f);
-        }
+        _isComplete = false;
     }
-    
-    private void TestAction()
+
+    private IEnumerator TestAction()
     {
-        // bag.transform.position = Vector3.zero;
+        // 멀티일 시 고유한 뭔가(ex. ID)를 찾은 뒤에 GetComponentInChildren으로 진행 예정
+        Vector3 originPos = FindObjectOfType<XRController>().transform.localPosition;
+        originPos.y += 0.5f;
+        _fixPos = originPos;
+
+        while (!_isComplete)
+        {
+            for (int i = 0; i < _bag.Length; i++)
+            {
+                if (Vector3.Distance(_bag[i].transform.position, _fixPos) < 0.1f)
+                {
+                    _bag[i].transform.position = _fixPos;
+                    _bag[i].transform.SetParent(_xrParent.transform);
+
+                    Rigidbody rb = _bag[i].GetComponent<Rigidbody>();
+                    rb.isKinematic = true;
+                }
+            }
+            
+            yield return null;
+        }
+        
         print("가방 고정됨");
+        _isComplete = true;
     }
 }
