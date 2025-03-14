63ecef33d3596f11e270d8c09b5d0747cc16046e
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-03-14 11:37:46 +0900

    [수정] NetworkXRGrabInteractable 수정

--- Assets/01_Scripts/Photon/NetworkXRGrabInteractable.cs
+++ Assets/01_Scripts/Photon/NetworkXRGrabInteractable.cs
@@ -3,22 +3,16 @@ using Fusion;
 using Fusion.Addons.Physics;
 using UnityEngine.XR.Interaction.Toolkit;
 
-[RequireComponent(typeof(XRGrabInteractable))]
-[RequireComponent(typeof(NetworkRigidbody3D))]
-[RequireComponent(typeof(NetworkObject))]
-[RequireComponent(typeof(Rigidbody))]
 public class NetworkedXRGrabInteractable : NetworkBehaviour
 {
-	[Networked] public NetworkBool IsGrabbed { get; set; } = false;
-
+	[Networked] private Vector3 Pos { get; set; }
+	[Networked] private Quaternion Rot { get; set; }
+	public bool isGrabbed = false;
 	private XRGrabInteractable _grabInteractable;
-	private Rigidbody _rb;
-	private Transform _currentInteractor;
 
 	private void Awake()
 	{
 		_grabInteractable = GetComponent<XRGrabInteractable>();
-		_rb = GetComponent<Rigidbody>();
 
 		_grabInteractable.selectEntered.AddListener(OnGrab);
 		_grabInteractable.selectExited.AddListener(OnRelease);
@@ -26,48 +20,26 @@ public class NetworkedXRGrabInteractable : NetworkBehaviour
 
 	private void OnGrab(SelectEnterEventArgs args)
 	{
-		if (Object.HasStateAuthority || Object.HasInputAuthority)
-		{
-			if (!Object.HasInputAuthority)
-			{
-				_currentInteractor = args.interactorObject.transform;
-				Object.AssignInputAuthority(Object.InputAuthority);
-			}
-
-			IsGrabbed = true;
-			_rb.isKinematic = true;
-		}
+		isGrabbed = true;
 	}
 
 	private void OnRelease(SelectExitEventArgs args)
 	{
-		if (Object.HasInputAuthority)
-		{
-			_currentInteractor = null;
-			Object.RemoveInputAuthority();
-			IsGrabbed = false;
-			_rb.isKinematic = false;
-		}
+		isGrabbed = false;
 	}
 
 	public override void FixedUpdateNetwork()
 	{
-		if (!IsGrabbed) return;
-
-		Follow(transform, _currentInteractor);
+		if (isGrabbed)
+		{
+			Pos = transform.position;
+			Rot = transform.rotation;
+		}
 	}
 
 	public override void Render()
 	{
-		if (IsGrabbed)
-		{
-			Follow(transform, _currentInteractor);
-		}
-	}
-
-	public void Follow(Transform followingTransform, Transform follwedTransform)
-	{
-		followingTransform.position = follwedTransform.position;
-		followingTransform.rotation = follwedTransform.rotation;
+		transform.position = Pos;
+		transform.rotation = Rot;
 	}
 }
