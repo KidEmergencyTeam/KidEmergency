9385b573ccc84d86c33f3e5284c57389a3210f4b
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-03-18 13:14:49 +0900

    [수정] JSWLobbyManager

--- Assets/01_Scripts/Photon/JSWLobbyManager.cs
+++ Assets/01_Scripts/Photon/JSWLobbyManager.cs
@@ -4,52 +4,48 @@ using UnityEngine.UI;
 
 public class JSWLobbyManager : MonoBehaviour
 {
-    public GameObject titleMenu;
-    public Button enterMultiKindergarten; 
-    public Button enterSingleKindergarten; 
-    public Button enterMultiSchool; 
-    public Button enterSingleSchool; 
-    public Button enterMultiPark; 
-    public Button enterSinglePark; 
+	public GameObject titleMenu;
+	public Button enterMultiKindergarten;
+	public Button enterSingleKindergarten;
+	public Button enterMultiSchool;
+	public Button enterSingleSchool;
+	public Button enterMultiPark;
+	public Button enterSinglePark;
 
-    private void Start()
-    {
-        titleMenu.SetActive(true);
-        enterMultiKindergarten.onClick.AddListener(EnterMultiKindergarten);
-        enterSingleKindergarten.onClick.AddListener(EnterSingleKindergarten);
-        enterMultiSchool.onClick.AddListener(EnterMultiSchool);
-        enterSingleSchool.onClick.AddListener(EnterSingleSchool);
-        enterMultiPark.onClick.AddListener(EnterMultiPark);
-        enterSinglePark.onClick.AddListener(EnterSinglePark);
-    }
-    
-    private void EnterMultiKindergarten()
-    {
-        titleMenu.SetActive(false);
-        SceneManager.LoadScene("JSW 1-1");
-    }
+	private void Start()
+	{
+		titleMenu.SetActive(true);
+		enterMultiKindergarten.onClick.AddListener(EnterMultiKindergarten);
+		enterSingleKindergarten.onClick.AddListener(EnterSingleKindergarten);
+		enterMultiSchool.onClick.AddListener(EnterMultiSchool);
+		enterSingleSchool.onClick.AddListener(EnterSingleSchool);
+		enterMultiPark.onClick.AddListener(EnterMultiPark);
+		enterSinglePark.onClick.AddListener(EnterSinglePark);
+	}
 
-    private void EnterSingleKindergarten()
-    {
-    }
+	private void EnterMultiKindergarten()
+	{
+		titleMenu.SetActive(false);
+		SceneManager.LoadScene("JSW 1-1");
+	}
 
-    private void EnterMultiSchool()
-    {
-        titleMenu.SetActive(false);
-        SceneManager.LoadScene("JSW 2-1");
-    }
+	private void EnterSingleKindergarten()
+	{
+	}
 
-    private void EnterSingleSchool()
-    {
-    }
+	private void EnterMultiSchool()
+	{
+	}
 
-    private void EnterMultiPark()
-    {
-        titleMenu.SetActive(false);
-        SceneManager.LoadScene("JSW 3-1");
-    }
+	private void EnterSingleSchool()
+	{
+	}
 
-    private void EnterSinglePark()
-    {
-    }
+	private void EnterMultiPark()
+	{
+	}
+
+	private void EnterSinglePark()
+	{
+	}
 }
