1ca8171a72987931d5cd6a44aca74ba41e304f8b
soyoung <soyoung7896@naver.com>
2025-03-18 12:06:11 +0900

    [수정] JSY Ray Controller Script

--- Assets/01_Scripts/JSY_DialogAction/JSYRayController.cs
+++ Assets/01_Scripts/JSY_DialogAction/JSYRayController.cs
@@ -8,6 +8,10 @@ public class JSYRayController : MonoBehaviour
     [SerializeField] private ActionBasedController _rightController;
     [SerializeField] private XRRayInteractor _leftRay;
     [SerializeField] private XRRayInteractor _rightRay;
+    [SerializeField] private XRInteractorLineVisual _leftLine;
+    [SerializeField] private XRInteractorLineVisual _rightLine;
+
+    [SerializeField] private bool isUIActive = false;
 
     private void Start()
     {
@@ -18,12 +22,15 @@ public class JSYRayController : MonoBehaviour
 
     private void Update()
     {
+
+        RayLineControl();
+
         // 오른쪽 레이가 켜져있는 상태에서 왼쪽 컨트롤러의 그립 버튼을 눌렀으면 왼쪽 레이로 스위치
         if (_leftController.selectAction.action.ReadValue<float>() > 0 && _rightRay.enabled)
         {
             SwitchLeftRay();
         }
-        
+
         // 왼쪽 레이가 켜져있는 상태에서 왼쪽 컨트롤러의 그립 버튼을 눌렀으면 오른쪽 레이로 스위치
         else if (_rightController.selectAction.action.ReadValue<float>() > 0 && _leftRay.enabled)
         {
@@ -35,15 +42,48 @@ public class JSYRayController : MonoBehaviour
         {
             _rightRay.enabled = true;
         }
-        
-        
+
+
         // 오른쪽 그립버튼을 누르고 있는 상태라면 왼쪽 레이 활성화
         else if (_rightController.selectAction.action.ReadValue<float>() > 0 && _rightRay.hasSelection)
         {
             _leftRay.enabled = true;
         }
+
+
     }
 
+    private bool UIActive()
+    {
+        for (int i = 0; i < UIManager.Instance.optionUI.Length; i++)
+        {
+            if (UIManager.Instance.optionUI[i].gameObject.activeSelf)
+            {
+                return true;
+            }
+        } 
+        
+        // 타이틀 ui도 추가해야 할듯?
+        
+        return false;
+    }
+
+
+    private void RayLineControl()
+    {
+        if (UIActive())
+        {
+            _leftLine.enabled = true;
+            _rightLine.enabled = true;
+        }
+        
+        else
+        {
+            _leftLine.enabled = false;
+            _rightLine.enabled = false;
+        }
+    }
+    
     private void SwitchLeftRay()
     {
         _rightRay.enabled = false;
