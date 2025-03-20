884091082355fb16abebb8a2388cbb161cbff388
soyoung <soyoung7896@naver.com>
2025-03-20 10:17:09 +0900

    [수정] Ready UI
    준비 UI 컨트롤러와 연결

--- Assets/01_Scripts/UI/ReadyUI.cs
+++ Assets/01_Scripts/UI/ReadyUI.cs
@@ -3,49 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ReadyUI : MonoBehaviour
 {
-    public int currentPeople;
-    private int _maxPeople = 8;
-
     public Image cirlceImage;
     public GameObject checkImage;
-    public TextMeshProUGUI peopleText;
-
-    private void Start()
-    {
-        // if(싱글) peopletext 비활성화
-    }
+    public string nextScene;
+    private bool _isReady = false;
+    [SerializeField] private ActionBasedController _leftCtrl;
+    [SerializeField] private ActionBasedController _rightCtrl;
 
     private void Update()
     {
-        peopleText.text = currentPeople + " / " + _maxPeople;
         SetPeopleReady();
 }
 
     private void SetPeopleReady()
     {
-        // if(멀티) if(플레이어가 준비 중이면) 컬러 변화 및 체크 이미지 활성화
-        // if(싱글) if(준비를 누르면) 컬러 변화 및 체크 이미지 활성화
-        
-        // 테스트용 키임
-        if (Input.GetKeyDown(KeyCode.F)) // 사람이 들어오면
-        {
-            if (currentPeople < _maxPeople)
-            {
-                currentPeople++;
-            }
-
-            else return;
-        }
-
-        if (Input.GetKeyDown(KeyCode.G)) // 준비 버튼(각 그립 버튼)을 눌렀을 때
+        if (_leftCtrl.selectAction.action.ReadValue<float>() > 0 && _rightCtrl.selectAction.action.ReadValue<float>() > 0) // 준비 버튼(각 그립 버튼)을 눌렀을 때
         {
             cirlceImage.color = Color.green;
             checkImage.SetActive(true);
+
+            _isReady = true;
+            StartCoroutine(WaitForGameStart());
+        }
+    }
+
+    private IEnumerator WaitForGameStart()
+    {
+        while (_isReady)
+        {
+            yield return new WaitForSeconds(2f);
+            SceneManager.LoadScene(nextScene);
         }
     }
 }
