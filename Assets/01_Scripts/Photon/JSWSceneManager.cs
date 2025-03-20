f93724eb52c4fa005f49a85c24a1b742aa001291
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-03-20 17:25:56 +0900

    [수정] 서버 관련 전부 삭제

--- Assets/01_Scripts/Photon/JSWSceneManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class JSWSceneManager : MonoBehaviour
-{
-    //TODO:: 씬마다 스크립터블 오브젝트 만들고 다음 씬 및 BGM 넣어놓기
-    // 멀티 씬 전환 방법은 ConnectionManager에 있는 SceneManager SwitchScene()호출하기
-}
--- Assets/01_Scripts/Photon/NetworkPlayer.cs
+++
@@ -1,7 +0,0 @@
-using Fusion;
-using UnityEngine;
-
-public class NetworkPlayer : NetworkBehaviour
-{
-    [Networked] public int Token {get;set;}
-}
