a42fd286575b9fbb6f72f5dfa8190f1ac372e1cc
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-03-22 16:46:19 +0900

    [수정] Grabbable Script 수정

--- Assets/01_Scripts/InteractableObject/Grabbable.cs
+++ Assets/01_Scripts/InteractableObject/Grabbable.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Outlinable), typeof(Rigidbody))]
 public class Grabbable : MonoBehaviour
 {
-	public bool isGrabbable = false;
+	public bool isGrabbable = true; //그랩 가능한 상태, 스토리 진행에 따라 조절
 	public Vector3 grabPosOffset;
 	public Vector3 grabRotOffset;
 
