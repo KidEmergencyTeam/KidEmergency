122be0bee2a6e5f62061946ea609ccc60065b7ef
JiwonEE2 <jiwonchichi1@gmail.com>
2025-03-25 13:43:13 +0900

    [수정] Player, BowState, DownState
    회전 시 오프셋으로 인한 오류 수정. 원래 코드는 복구

--- Assets/01_Scripts/Player/States/DownState.cs
+++ Assets/01_Scripts/Player/States/DownState.cs
@@ -13,44 +13,39 @@ public class DownState : State
 	{
 		float downThreshold = player.downThreshold;
 		float downYValue = player.downYValue;
+
+		// 현재 헤드셋(카메라)의 Y 좌표 가져오기
 		float headHeight = Camera.main.transform.position.y;
 
-		Vector3 downOffset =
-			player.transform.InverseTransformDirection(Vector3.up * downYValue);
-
+		// 기준보다 낮아지면 숙이기
 		if (headHeight < downThreshold && !isDown)
 		{
 			isDown = true;
-
-			// 월드 기준으로 이동하여 회전 영향을 최소화
-			player.xrOrigin.position -= Vector3.up * downYValue;
-
-			// 발 위치 조정 (회전 보정 적용)
-			player.leftFootIkTarget.position += downOffset;
-			player.rightFootIkTarget.position += downOffset;
-			player.leftFootTarget.position += downOffset;
-			player.rightFootTarget.position += downOffset;
-			player.bodyTarget.position -= downOffset;
-
-			// 팔 IK 보정 (회전 보정 적용)
-			player.playerTargetFollower.followTargets[2].posOffset += downOffset;
-			player.playerTargetFollower.followTargets[3].posOffset += downOffset;
+			player.xrOrigin.localPosition -= new Vector3(0, downYValue, 0);
+			player.leftFootIkTarget.localPosition += new Vector3(0, downYValue, 0);
+			player.rightFootIkTarget.localPosition += new Vector3(0, downYValue, 0);
+			player.leftFootTarget.localPosition += new Vector3(0, downYValue, 0);
+			player.rightFootTarget.localPosition += new Vector3(0, downYValue, 0);
+			player.bodyTarget.localPosition -= new Vector3(0, downYValue, 0);
+			player.playerTargetFollower.followTargets[2].posOffset +=
+				new Vector3(0, downYValue, 0);
+			player.playerTargetFollower.followTargets[3].posOffset +=
+				new Vector3(0, downYValue, 0);
 		}
+		// 일정 높이 이상 올라가면 다시 서기
 		else if (headHeight > downThreshold - downYValue && isDown)
 		{
 			isDown = false;
-
-			// 원래 위치로 복귀
-			player.xrOrigin.position += Vector3.up * downYValue;
-
-			player.leftFootIkTarget.position -= downOffset;
-			player.rightFootIkTarget.position -= downOffset;
-			player.leftFootTarget.position -= downOffset;
-			player.rightFootTarget.position -= downOffset;
-			player.bodyTarget.position += downOffset;
-
-			player.playerTargetFollower.followTargets[2].posOffset -= downOffset;
-			player.playerTargetFollower.followTargets[3].posOffset -= downOffset;
+			player.xrOrigin.localPosition += new Vector3(0, downYValue, 0);
+			player.leftFootIkTarget.localPosition -= new Vector3(0, downYValue, 0);
+			player.rightFootIkTarget.localPosition -= new Vector3(0, downYValue, 0);
+			player.leftFootTarget.localPosition -= new Vector3(0, downYValue, 0);
+			player.rightFootTarget.localPosition -= new Vector3(0, downYValue, 0);
+			player.bodyTarget.localPosition += new Vector3(0, downYValue, 0);
+			player.playerTargetFollower.followTargets[2].posOffset -=
+				new Vector3(0, downYValue, 0);
+			player.playerTargetFollower.followTargets[3].posOffset -=
+				new Vector3(0, downYValue, 0);
 		}
 	}
 
