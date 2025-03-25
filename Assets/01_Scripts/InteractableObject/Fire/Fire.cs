76887b969dfd74655a4202458b6bab6d027a8c0e
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-03-25 21:04:55 +0900

    [수정] Fire Script 수정

--- Assets/01_Scripts/InteractableObject/Fire/Fire.cs
+++ Assets/01_Scripts/InteractableObject/Fire/Fire.cs
@@ -1,6 +1,20 @@
+using System;
 using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
 	public float fireHp = 100f;
+	public Vector3 originalScale;
+
+	private void Awake()
+	{
+		originalScale = transform.localScale;
+	}
+
+	public void TakeDamage(float damage)
+	{
+		fireHp -= damage;
+		if (fireHp < 10) Destroy(gameObject);
+		transform.localScale = originalScale * fireHp / 100f;
+	}
 }
