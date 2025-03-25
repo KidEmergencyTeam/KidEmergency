c6d3421e02fc128e69af831764e0b2260225c4e0
soyoung <soyoung7896@naver.com>
2025-03-25 15:05:33 +0900

    [추가] EndGameAction

---
+++ Assets/01_Scripts/JSY_DialogAction/Action/Actions/EndGameAction.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameAction : MonoBehaviour, IActionEffect
+{
+    public bool IsActionComplete { get; }
+
+    public void StartAction()
+    {
+        BackToStartScene();
+    }
+
+    private void BackToStartScene()
+    {
+        Destroy(ActionManager.Instance.gameObject);
+        Destroy(FadeInOut.Instance.gameObject);
+        Destroy(UIManager.Instance.gameObject);
+        SceneManager.LoadScene("JSY_Start", LoadSceneMode.Single);
+    }
+
+}
