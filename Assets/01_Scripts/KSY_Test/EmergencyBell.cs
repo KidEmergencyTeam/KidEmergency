7ad76b9c61c055a5a302646ff3bfe59727e425e8
ztazan2 <ztazan2@gamil.com>
2025-03-25 15:33:53 +0900

    [수정] EmergencyBell.cs
    경고벨 직접 호출 x -> 단순히 클릭 했다는 결과만 보내는 개념

--- Assets/01_Scripts/KSY_Test/EmergencyBell.cs
+++ Assets/01_Scripts/KSY_Test/EmergencyBell.cs
@@ -1,23 +1,27 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EmergencyBell : MonoBehaviour
+public class EmergencyBellButton : MonoBehaviour
 {
+    [Header("EmergencyBell 버튼")]
     public Button emergencyBell;
 
+    // 버튼 클릭 시 다른 스크립트에 알리기 위한 이벤트
+    public static event Action OnEmergencyBellClicked;
+
+    // 버튼 처리
     private void Start()
     {
         if (emergencyBell != null)
-        {
-            // 버튼 클릭 시 경보벨 발생
-            emergencyBell.onClick.AddListener(() =>
-            {
-                TypingEffect.Instance.StartContinuousSeparateTypingClip();
-            });
-        }
+            emergencyBell.onClick.AddListener(HandleButtonClick);
         else
-        {
-            Debug.LogWarning("emergencyBell이 할당되지 않았습니다.");
-        }
+            Debug.LogError("EmergencyBell 버튼이 할당되지 않았습니다.");
+    }
+
+    // 버튼 클릭 시 실행
+    private void HandleButtonClick()
+    {
+        OnEmergencyBellClicked?.Invoke();
     }
 }
