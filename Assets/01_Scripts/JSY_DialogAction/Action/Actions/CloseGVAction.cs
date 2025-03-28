1fc523bf496677287e0b74192d5af614b0034451
soyoung <soyoung7896@naver.com>
2025-03-28 10:49:49 +0900

    [추가] Close Gas Valve Action
    및 Lower Circuit Lever / Open Circuit Box 액션 스크립트 추가

---
+++ Assets/01_Scripts/JSY_DialogAction/Action/Actions/CloseGVAction.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseGVAction : MonoBehaviour, IActionEffect
+{
+
+    public void StartAction()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool IsActionComplete { get; }
+}
