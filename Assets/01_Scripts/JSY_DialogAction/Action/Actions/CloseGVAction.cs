e9efdcf12b17afb39327826858ccd7cf08773246
soyoung <soyoung7896@naver.com>
2025-03-28 15:41:39 +0900

    [수정] CloseGVAction

--- Assets/01_Scripts/JSY_DialogAction/Action/Actions/CloseGVAction.cs
+++ Assets/01_Scripts/JSY_DialogAction/Action/Actions/CloseGVAction.cs
@@ -1,14 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class CloseGVAction : MonoBehaviour, IActionEffect
 {
+    public GameObject target; // 가스 밸브
+
+    private XRGrabInteractable _grab;
+    private float _startAngle;
+    private float _currentAngle;
+    private float _limitAngle = 180f;
+
+    [SerializeField] private bool _isComplete = false;
+    public bool IsActionComplete => _isComplete;
+
+    private void Awake()
+    {
+        _grab = GetComponentInChildren<XRGrabInteractable>();
+    }
+
+    private void Start()
+    {
+        _startAngle = target.transform.eulerAngles.z;
+        StartCoroutine(TryCloseGV());
+    }
+
+    private void Update()
+    {
+        if (target != null && _isComplete)
+        {
+            target.transform.localEulerAngles = new Vector3(0,180,_limitAngle);
+        }
+    }
 
     public void StartAction()
     {
-        throw new System.NotImplementedException();
+        StartCoroutine(TryCloseGV());
     }
 
-    public bool IsActionComplete { get; }
+    private IEnumerator TryCloseGV()
+    {
+        while (!_isComplete)
+        {
+            print("코루틴 시작");
+            _grab.enabled = true;
+            _currentAngle = target.transform.eulerAngles.z;
+            float clampedAngle = Mathf.Clamp(_currentAngle, _startAngle, _startAngle + 90);
+
+            target.transform.localEulerAngles = new Vector3(0,180, clampedAngle);
+
+            if (_currentAngle >= _limitAngle)
+            {
+                _isComplete = true;
+                _grab.enabled = false;
+            }
+            
+            yield return null;
+        }
+    }
+
 }
