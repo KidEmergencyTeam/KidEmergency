121f9e3f6b229a2a369b9acebb516a353bf81276
ztazan2 <ztazan2@gamil.com>
2025-03-28 13:58:44 +0900

    [수정] 주석 정리 완료 -> 씬 이동 시 손수건 유지 되도록 구현

--- Assets/01_Scripts/KSY_Test/GrabStatePersistence.cs
+++ Assets/01_Scripts/KSY_Test/GrabStatePersistence.cs
@@ -13,27 +13,31 @@ public class GrabStatePersistence : DisableableSingleton<GrabStatePersistence>
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    // 씬 로드 후, Hand 태그를 가진 새로운 손 객체에서 Grabber를 갱신
+    // 씬 로드 후, Hand 태그를 가진 새로운 Left Controller 오브젝트에 추가된 Grabber 컴포넌트를 찾아
+    // 손수건 오브젝트에 추가된 Grabbable 컴포넌트에서 currentGrabber를 갱신
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         if (grabbableComponent != null)
         {
-            // Hand 태그를 가진 새 손(Grabber) 오브젝트를 찾기
+            // Hand 태그를 가진 새로운 Left Controller 오브젝트 찾기
             GameObject handObj = GameObject.FindGameObjectWithTag("Hand");
             if (handObj == null)
             {
-                Debug.LogWarning("새로운 Grabber(Hand 태그)를 찾을 수 없습니다.");
+                Debug.LogWarning("Hand 태그의 Left Controller 오브젝트를 찾을 수 없습니다.");
                 return;
             }
 
             Grabber newGrabber = handObj.GetComponent<Grabber>();
             if (newGrabber == null)
             {
-                Debug.LogWarning("Hand 태그의 객체에서 Grabber 컴포넌트를 찾을 수 없습니다.");
+                Debug.LogWarning("Hand 태그의 Left Controller 오브젝트에서 Grabber 컴포넌트를 찾을 수 없습니다.");
                 return;
             }
 
-            // grabbed object의 currentGrabber를 갱신하기
+            // 손수건 오브젝트에 추가된
+            // Grabbable 컴포넌트에서 currentGrabber를 갱신하면
+            // Grabbable 컴포넌트에서 손수건의 위치와 방향을
+            // Grabber 컴포넌트가 추가된 오브젝트의 위치와 방향으로 처리
             grabbableComponent.currentGrabber = newGrabber;
         }
     }
