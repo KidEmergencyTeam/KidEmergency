0f0825d130de515b6284148ff91e30faadb65255
ztazan2 <ztazan2@gamil.com>
2025-04-02 17:33:06 +0900

    [추가] 페이드 인 아웃 이미지 전달

---
+++ Assets/01_Scripts/KSY_Test/SendFadeImage.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+// fadeInoutImg 전달
+public class SendFadeImage : MonoBehaviour
+{
+    [Header("페이드 인 아웃 이미지")]
+    [SerializeField] private Image fadeInoutImg;
+
+    private void Awake()
+    {
+        if (fadeInoutImg == null)
+        {
+            Debug.LogError("[SendFadeImage] 페이드 인 아웃 이미지가 할당되지 않음");
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        if (FadeInOut.Instance != null)
+        {
+            FadeInOut.Instance.SetFadeImage(fadeInoutImg);
+        }
+        else
+        {
+            Debug.LogError("[SendFadeImage] FadeInOut 인스턴스를 찾을 수 없음");
+        }
+    }
+}
