d2ce2817d8181271aa263d304b14cb3d0e239265
soyoung <soyoung7896@naver.com>
2025-04-03 17:32:55 +0900

    [수정] DeskLeg
    변경된 아웃라인에 맞게 코드 수정

--- Assets/01_Scripts/JSY_DialogAction/BaseOutlineObject.cs
+++ Assets/01_Scripts/JSY_DialogAction/BaseOutlineObject.cs
@@ -5,29 +5,24 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class BaseOutlineObject : MonoBehaviour
 {
-     private Outlinable _outlinable;
-     private Color _originColor;
+     public Highlighter _highlighter;
 
      [SerializeField] private GameObject _leftHand;
      [SerializeField] private GameObject _rightHand;
      
-     private void Start()
-    {
-        _outlinable = GetComponent<Outlinable>();
-        _originColor = _outlinable.OutlineParameters.Color;
-    }
-
     private void Update()
     {
-        if(Vector3.Distance(this.gameObject.transform.position, _leftHand.transform.position) < 0.05f
-           || Vector3.Distance(this.gameObject.transform.position, _rightHand.transform.position) < 0.05f)
+        if(Vector3.Distance(this.gameObject.transform.position, _leftHand.transform.position) < 0.1f
+           || Vector3.Distance(this.gameObject.transform.position, _rightHand.transform.position) < 0.1f)
         {
-            _outlinable.OutlineParameters.Color = Color.green;
+            _highlighter.SetColor(Color.green);
+            _highlighter.isBlinking = false;
         }
 
         else
         {
-            _outlinable.OutlineParameters.Color = _originColor;
+            _highlighter.SetColor(Color.yellow);
+            _highlighter.isBlinking = true;
         }
     }
     
--- Assets/01_Scripts/JSY_DialogAction/DeskLeg.cs
+++ Assets/01_Scripts/JSY_DialogAction/DeskLeg.cs
@@ -12,23 +12,17 @@ public class DeskLeg : MonoBehaviour
     [SerializeField] private GameObject[] _legs;
     [SerializeField] private GameObject _leftHand;
     [SerializeField] private GameObject _rightHand;
+    [SerializeField] private GameObject[] _legsHighlight;
     
     private HandAnimation _handAnimation;
     private ActionBasedController _leftController;
     private ActionBasedController _rightController;
     private float _durationTime = 0f; // 지속 시간
     private float _endTime = 5f; // 종료 시간
-    private bool _isTrigger = false;
     public bool isHoldComplete = false;
 
     private void Awake()
     {
-        for (int i = 0; i < _legs.Length; i++)
-        {
-            _legs[i].GetComponent<Outlinable>().enabled = false;
-            _legs[i].GetComponent<BaseOutlineObject>().enabled = false;
-        }
-        
         _leftController = GameObject.Find("Left Controller").GetComponent<ActionBasedController>();
         _rightController = GameObject.Find("Right Controller").GetComponent<ActionBasedController>();
     }
@@ -46,14 +40,24 @@ public class DeskLeg : MonoBehaviour
 
     public void Holding()
     {
+        for (int i = 0; i < _legsHighlight.Length; i++)
+        {
+            _legsHighlight[i].SetActive(true);
+        }
+        
+        for (int i = 0; i < _legs.Length; i++)
+        {
+            _legs[i].GetComponent<BaseOutlineObject>().enabled = true;
+        }
+        
         bool isLeftGrapped = _leftController.selectAction.action.ReadValue<float>() > 0;
         if(isLeftGrapped) _handAnimation.animator.SetFloat("Left Trigger", 1);
         bool isRightGrapped = _rightController.selectAction.action.ReadValue<float>() > 0;
         if(isRightGrapped) _handAnimation.animator.SetFloat("Right Trigger", 1);
         // bool isInteractable =
         //     Vector3.Distance(_legs[0].transform.position, _leftHand.transform.position) < 0.05f && isLeftGrapped;
-        bool isInteractable = Vector3.Distance(_legs[0].transform.position, _leftController.transform.position) < 0.05f && isLeftGrapped &&
-                                  Vector3.Distance(_legs[1].transform.position, _rightController.transform.position) < 0.05f && isRightGrapped;
+        bool isInteractable = Vector3.Distance(_legs[0].transform.position, _leftController.transform.position) < 0.1f && isLeftGrapped &&
+                                  Vector3.Distance(_legs[1].transform.position, _rightController.transform.position) < 0.1f && isRightGrapped;
         if (isInteractable)
         { 
             UIManager.Instance.CloseWarningUI();
@@ -86,20 +90,9 @@ public class DeskLeg : MonoBehaviour
 
     public void RemoveOutline()
     {
-        Outlinable[] outlinables = GetComponentsInChildren<Outlinable>();
-        foreach (Outlinable outline in outlinables)
+        for (int i = 0; i < _legsHighlight.Length; i++)
         {
-            outline.enabled = false;
+            _legsHighlight[i].SetActive(false);
         }
-    }
-
-    public void TriggerLeg()
-    {
-        _isTrigger = true;
-    }
-
-    public void UnTriggerLeg()
-    {
-        _isTrigger = false;
     }
 }
