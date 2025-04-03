2c5d149cbda96f4a46d4ba3aa8d755a431f38229
ztazan2 <ztazan2@gamil.com>
2025-04-03 10:13:42 +0900

    [수정] 메인 카메라 높이 -0.2일 때만 디버그 출력

--- Assets/01_Scripts/KSY_Test/VRCameraHeightMonitor.cs
+++ Assets/01_Scripts/KSY_Test/VRCameraHeightMonitor.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 
-// 메인 카메라 높이 값 출력
+// 높이 측정
 public class VRCameraHeightMonitor : MonoBehaviour
 {
+    [Header("메인 카메라")]
     public Transform mainCamera;
+
+    // 메인 카메라 높이가
+    // 목표 높이에 도달할 때만 디버그 출력
+    [Header("목표 높이")]
+    public float targetHeight = -0.2f; 
 
     void Update()
     {
         if (mainCamera != null)
         {
             float localHeight = mainCamera.localPosition.y;
-            Debug.Log("메인 카메라 높이: " + localHeight);
+
+            // 메인 카메라 높이가
+            // 목표 높이와 같은지 확인
+            if (Mathf.Approximately(localHeight, targetHeight))
+            {
+                Debug.Log("메인 카메라 높이: " + localHeight);
+            }
         }
         else
         {
