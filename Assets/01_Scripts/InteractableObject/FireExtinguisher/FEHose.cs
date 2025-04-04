03d6d7989a58b79ec0e34833a3e24f8952995c36
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-04-04 18:02:33 +0900

    [수정] FEHose Script 수정

--- Assets/01_Scripts/InteractableObject/FireExtinguisher/FEHose.cs
+++ Assets/01_Scripts/InteractableObject/FireExtinguisher/FEHose.cs
@@ -1,13 +1,40 @@
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 
-public class FEHose : MonoBehaviour
+public class FEHose : Grabbable
 {
-	public Grabbable grabbable;
 	public ChainIKConstraint hoseIKChain;
+	public Transform grabPoint;
 
-	private void Start()
+	protected override void Update()
 	{
-		grabbable = GetComponent<Grabbable>();
+		if (!isGrabbable)
+		{
+			highlight.SetActive(false);
+
+			if (!IsGrabbed) return;
+
+			if (isMoving)
+			{
+				realMovingObject.transform.position =
+					grabPoint.position + grabPosOffset;
+				realMovingObject.transform.rotation =
+					grabPoint.rotation * Quaternion.Euler(grabRotOffset);
+			}
+		}
+		else
+		{
+			highlight.SetActive(true);
+			if (isTrigger)
+			{
+				highlighter.SetColor(Color.green);
+				highlighter.isBlinking = false;
+			}
+			else
+			{
+				highlighter.SetColor(Color.yellow);
+				highlighter.isBlinking = true;
+			}
+		}
 	}
 }
