23d6e26e232898dafc3b0fb9b49224f9ab339384
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-04-09 13:47:20 +0900

    [수정] FEEndState Script 수정

--- Assets/01_Scripts/FireExtinguisher/FEState/FEEndState.cs
+++ Assets/01_Scripts/FireExtinguisher/FEState/FEEndState.cs
@@ -1,7 +1,10 @@
+using UnityEngine.SceneManagement;
+
 public class FEEndState : FEState
 {
 	public override void EnterState(FEScene scene)
 	{
+		SceneManager.LoadScene(0);
 	}
 
 	public override void ExecuteState(FEScene scene)
