c0e6b0d0f68a28237b2b418aa69dc05f9a4849eb
ztazan2 <ztazan2@gamil.com>
2025-04-09 16:23:41 +0900

    [추가] 안전 유도성 깜빡이 제어하는 스크립트

--- Assets/01_Scripts/KSY_Test/ToggleHighlighter.cs
+++ Assets/01_Scripts/KSY_Test/ToggleHighlighter.cs
@@ -3,22 +3,31 @@ using UnityEngine;
 // 안전 유도선 깜빡이 켜는 스크립트
 public class ToggleHighlighter : MonoBehaviour
 {
-    private Highlighter highlighter;
-
-    private void Start()
-    {
-        ToggleHighlighter toggleHighlighter = GameObject.FindGameObjectWithTag("ToggleHighlighter")?.GetComponent<ToggleHighlighter>();
-
-        if (toggleHighlighter == null)
-        {
-            Debug.LogError("ToggleHighlighter 컴포넌트를 찾을 수 없습니다.");
-        }
-    }
-
-    // Highlighter 켜기
+    // 씬에 존재하는 모든 Highlighter 스크립트를 찾아서 색상을 변경하고 깜빡임을 활성화
     public void Toggle()
     {
-        highlighter.SetColor(Color.yellow);
-        highlighter.isBlinking = true;
+        // "SafetyLine" 태그가 붙은 오브젝트 찾아서 배열에 담기
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("SafetyLine");
+
+        if (objects.Length == 0)
+        {
+            Debug.LogWarning("오브젝트를 찾을 수 없습니다.");
+            return;
+        }
+
+        // Highlighter 컴포넌트에 접근하여 설정 적용
+        foreach (GameObject obj in objects)
+        {
+            Highlighter highlighter = obj.GetComponent<Highlighter>();
+            if (highlighter != null)
+            {
+                highlighter.SetColor(Color.yellow);
+                highlighter.isBlinking = true;
+            }
+            else
+            {
+                Debug.LogWarning("해당 게임 오브젝트에서 Highlighter 컴포넌트를 찾을 수 없습니다: " + obj.name);
+            }
+        }
     }
 }
