ab2e8bf040a3593cedde4765cf107d8ce3b2a78b
soyoung <soyoung7896@naver.com>
2025-04-10 10:36:03 +0900

    [수정] Close GV Action

--- Assets/01_Scripts/JSY_DialogAction/Action/Actions/CloseGVAction.cs
+++ Assets/01_Scripts/JSY_DialogAction/Action/Actions/CloseGVAction.cs
@@ -1,28 +1,24 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class CloseGVAction : MonoBehaviour, IActionEffect
 {
-    [SerializeField] private GameObject _target; // 가스 밸브
+    [SerializeField] private GameObject _target; 
     [SerializeField] private GameObject[] _hand; // 0 왼, 1 오
     [SerializeField] private GameObject _highlighter;
+    [SerializeField] private float _limitRot = 90f;
+    [SerializeField] private XRKnob _knob;
     
-    private float _limitRot = 90f;
-    private XRGrabInteractable _grabInteractable;
     private bool _isComplete = false;
-    
+
     public bool IsActionComplete => _isComplete;
-
-    private void Awake()
-    {
-        _grabInteractable = _target.GetComponent<XRGrabInteractable>();
-    }
-
+    
     private void Start()
     {
-        _grabInteractable.enabled = false;
-        _target.GetComponent<BaseOutlineObject>().enabled = false;
+        _knob.enabled = false;
     }
 
     public void StartAction()
@@ -33,36 +29,27 @@ public class CloseGVAction : MonoBehaviour, IActionEffect
 
     private IEnumerator TryCloseGV()
     {
+        _highlighter.SetActive(true);
+        _target.GetComponent<BaseOutlineObject>().enabled = true;
+        
         while (!_isComplete)
         {
-            // _highlighter.SetActive(true);
-            // _target.GetComponent<BaseOutlineObject>().enabled = true;
-            // bool isInteractable = Vector3.Distance(_target.transform.position, _hand[0].transform.position) < 0.05f
-            //                       || Vector3.Distance(_target.transform.position, _hand[1].transform.position) < 0.05f;
-            // if (isInteractable)
-            // {
-            //     _grabInteractable.enabled = true;
-            //     
-            //     float currentZRotation = _target.transform.rotation.eulerAngles.z;
-            //
-            //     if (currentZRotation == _limitRot)
-            //     {
-            //         _grabInteractable.enabled = false;
-            //         _target.transform.rotation = Quaternion.Euler(0, 0, _limitRot);
-            //
-            //         _target.GetComponent<BaseOutlineObject>().enabled = false;
-            //         _highlighter.SetActive(false);
-            //         _isComplete = true;
-            //     }
-            // }
-            //
-            // else
-            // {
-            //     _grabInteractable.enabled = false;
-            // }
-            _isComplete = true;
-            yield return null;
-
+            bool isInteractable = Vector3.Distance(_target.transform.position, _hand[0].transform.position) < 0.1f ||
+                                  Vector3.Distance(_target.transform.position, _hand[1].transform.position) < 0.1f;
+            if (isInteractable)
+            {
+                _knob.enabled = true;
+                if(_knob.value >= 0.99f)
+                { 
+                    _knob.enabled = false;
+                    _knob.value = 1f;
+                    _target.GetComponent<BaseOutlineObject>().enabled = false;
+                    _highlighter.SetActive(false);
+                        
+                    _isComplete = true;
+                } 
+            }
+            yield return null; 
         }
     }
 }
