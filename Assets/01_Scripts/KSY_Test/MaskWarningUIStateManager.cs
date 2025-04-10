2e1c9fe9c913b03e83804f9ff82da80e9db01bdd
ztazan2 <ztazan2@gamil.com>
2025-04-10 14:06:38 +0900

    [수정] 주석 정리 및 불필요한 매개 변수 제거

--- Assets/01_Scripts/KSY_Test/MaskWarningUIStateManager.cs
+++ Assets/01_Scripts/KSY_Test/MaskWarningUIStateManager.cs
@@ -9,22 +9,18 @@ public class MaskWarningUIStateManager : DisableableSingleton<MaskWarningUIState
     // 충돌 상태 저장하기
     public void SetCollisionState(bool setState)
     {
-        // 내부 변수에 충돌 상태를 저장
+        // 내부 변수에 충돌 상태 저장
         isCollided = setState;
         Debug.Log("충돌 상태 저장 완료");
     }
 
     // 충돌 상태 불러오기
-    public bool GetCollisionState(bool getState)
+    public bool GetCollisionState()
     {
-        bool State = isCollided;
+        // 내부 변수에 저장된 충돌 상태 불러오기
+        bool currentState = isCollided;
 
-        // 전달받은 값이 true면 -> false
-        if (getState)
-        {
-            isCollided = false;
-            Debug.Log("[MaskWarningUIStateManager] GetCollisionState 호출 후 상태가 초기화되었습니다.");
-        }
-        return State;
+        // 내부 변수에 저장된 충돌 상태 반환
+        return currentState;
     }
 }
