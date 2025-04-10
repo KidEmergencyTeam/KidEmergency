1c45a13839c41119de7b4e902fc0bf85fd90b0b4
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-04-10 10:07:06 +0900

    [수정] FEEndState Script 수정

--- Assets/01_Scripts/FireExtinguisher/FEState/FEEndState.cs
+++ Assets/01_Scripts/FireExtinguisher/FEState/FEEndState.cs
@@ -1,14 +1,20 @@
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FEEndState : FEState
 {
+	private float startTime;
+	private float delay = 1f;
+
 	public override void EnterState(FEScene scene)
 	{
-		SceneManager.LoadScene(0);
+		startTime = Time.time;
 	}
 
 	public override void ExecuteState(FEScene scene)
 	{
+		if (startTime + delay < Time.time) scene.FadeOut();
+		if (startTime + delay + OVRScreenFade.Instance.fadeTime < Time.time) SceneManager.LoadScene(0);
 	}
 
 	public override void ExitState(FEScene scene)
