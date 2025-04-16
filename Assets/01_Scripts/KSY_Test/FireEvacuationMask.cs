5601d55a2adbaec6f017ca2fcd88dc3a4438a36f
ztazan2 <ztazan2@gamil.com>
2025-04-16 13:53:30 +0900

    [수정] 손수건을 잡은 상태일 때 입 콜라이더의 trigger를 활성화하고, 잡지 않은 상태일 때 비활성화

--- Assets/01_Scripts/KSY_Test/FireEvacuationMask.cs
+++ Assets/01_Scripts/KSY_Test/FireEvacuationMask.cs
@@ -7,11 +7,38 @@ public class FireEvacuationMask : MonoBehaviour
     [Header("충돌 대상 태그 (손수건)")]
     public string targetTag = "Handker";
 
+    [Header("입 콜라이더")]
+    public Collider myCollider;
+
+    [Header("Grabber")]
+    public Grabber myGrabber;
+
     // 손수건과 충돌할 때 다른 스크립트에 알리기 위한 이벤트
     public event Action OnHandkerchiefEnter;
 
     // 손수건과 충돌 종료할 때 다른 스크립트에 알리기 위한 이벤트
     public event Action OnHandkerchiefExit;
+
+    private void Update()
+    {
+        EnableTriggerMode();
+    }
+
+    // Trigger 켜기 및 끄기
+    public void EnableTriggerMode()
+    {
+        // 손수건을 잡고 있다면 트리거 켜기
+        if(myGrabber.currentGrabbedObject != null)
+        {
+            myCollider.isTrigger = true;
+        }
+
+        // 아니라면 트리거 끄기
+        else
+        {
+            myCollider.isTrigger = false;
+        }
+    }
 
     // 콜라이더가 충돌할 때 호출
     private void OnTriggerEnter(Collider other)
@@ -36,3 +63,5 @@ public class FireEvacuationMask : MonoBehaviour
         }
     }
 }
+
+// 콜라이더가 처음에 비활성화라 -> 잡았을때만 활성화 상태 전환
